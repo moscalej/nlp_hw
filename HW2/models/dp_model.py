df11ee5b4579b825a2a6d4302d3129ebb9aa560f
Alex Finkelshtein <alex.finkelshtein@intel.com>
2019-01-12 20:08:18 +0200

    DP_model partial implementation

--- HW2/models/dp_model.py
+++ HW2/models/dp_model.py
@@ -9,55 +9,67 @@ from models.chu_liu import Digraph
 class DP_Model:
 
     def __init__(self, num_features, boot_camp, tagger, w=None):
-        w = np.random.rand(num_features) if w is None else w
+        self.w = np.zeros(num_features) if w is None else w
         self.bc = boot_camp  # defines feature space
         self.tagger = tagger  # taging function
         assert (num_features, w.shape[0])  # make sure w has right dims
-        self.lr
+        self.lr = 1  # TODO
         pass
 
-    def fit(self, objs, epochs):
-        f_x = [obj.f for obj in objs]  # TODO: make sure passing an argument like this is really by pointer
-        y = [obj.graph for obj in objs]
+    def fit(self, obj_list, epochs):
+        self.bc.train_soldiers(obj_list)  # create f_x for each
+        f_x = (obj.f for obj in obj_list)  # TODO: Generator
+        # TODO: make sure passing an argument like this is really by pointer
+        y = (obj.graph for obj in obj_list)
         #
         self.perceptron(f_x, y, epochs)
 
-    def predict(self, x):
+    def predict(self, obj_list):
         """
 
-        :param x: iterable list
+        :param x: iterable list of sentences
         :type x:
         :return:
         :rtype:
         """
-        tags = self.tagger(x)
-        obj_list = []
-        for ind, sentence in enumerate(x):
-            obj_list.append(DP_sentence(sentence=sentence, tags=tags[ind]))
-        self.bc.train_soldiers(x)  # create f_x for each
+        # obj_list = []
+        # for ind, sentence in enumerate(x):
+        #     obj_list.append(DP_sentence(sentence=sentence, tags=tags[ind]))
+        self.bc.train_soldiers(obj_list)  # create f_x for each
         for obj in obj_list:
-            weighted_graph = self.create_full_tree(obj.f)
-            obj.graph = Digraph(
-                weighted_graph).greedy().successors  # TODO: currently only pseudo apply chu liu to object
+            full_graph, weight_dict = self.create_full_graph(obj.f)
+            get_score = lambda i, j: weight_dict[i, j]
+            obj.graph = Digraph(full_graph, get_score=get_score).greedy().successors
         result = [obj.graph for obj in obj_list]
         return result
 
     def perceptron(self, f_x_list, y, epochs):
         for i in range(epochs):
-            for ind, f_x in enumerate(f_x_list):
-                weighted_graph = self.create_full_graph(f_x)
-                opt_graph = Digraph(weighted_graph).greedy().successors  # TODO: currently only pseudo
+            for (f_x, graph) in zip(f_x_list, y):
+                full_graph, weight_dict = self.create_full_graph(f_x)
+                get_score = lambda i, j: weight_dict[i, j]
+                opt_graph = Digraph(full_graph,
+                                    get_score=get_score).greedy().successors
 
-                if opt_graph != y[ind]:  # TODO: currently only pseudo
-                    self.w = self.w + self.lr * (y[ind] - opt_graph)  # TODO: see
+                if opt_graph != graph:  # TODO: currently only pseudo
+                    self.w = self.w + self.lr * (graph - opt_graph)  # TODO: see how to define this substraction
 
     def create_full_graph(self, f_x):
         """
         Create full weighted graph for chu liu
         :param f_x:
         :type f_x: tensor
-        :return: full weighted graph
+        :return: full_graph and weighted matrix
         :rtype:
         """
+        # f_x dims: edge_source  x features x edge_target( edge source = edge_target +1 [root])
+        full_graph = {src: range(f_x.shape[2]) for src in range(f_x.shape[0])}
+        results = []
+        for src_feat_slice in f_x:
+            t = src_feat_slice.dot(self.w)
+            results.append(t)
+        weight_mat = np.array(results)
         # self.w
+        # dot product with self.w then we get a matrix describing the graphs weighted edges
+        return full_graph
         pass
