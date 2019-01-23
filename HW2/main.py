368e3302a454c21c76598c396b3196df0d634ae4
amoscoso <moscalej@gmail.com>
2019-01-23 23:48:04 +0200

    !!!!C-1!!!!

--- HW2/main.py
+++ HW2/main.py
@@ -26,9 +26,9 @@ results_all = []
 
 data = PreProcess(DATA_PATH).parser()
 test = PreProcess(TEST_PATH).parser()
-unlabel = PreProcess(paths['unlable_data'],label=False).parser()
+unlabel = PreProcess(paths['unlable_data'], label=False).parser()
 # BASE MODEL
-bc = BootCamp(Features('Advanse'))
+bc = BootCamp(Features('base'))  # base advance
 model = DP_Model(boot_camp=bc)
 
 start_time = time.time()
@@ -42,18 +42,17 @@ TRUNCATE_TOP = 0.03
 TRUNCATE_BOT = 0.10
 remove_top = int(total * TRUNCATE_TOP)
 remove_bot = int(total * TRUNCATE_BOT)
-model.bc.features.truncate_by_thresh(15_000,1)
+model.bc.features.truncate_by_thresh(15_000, 1)
 # model.bc.truncate_features(n_top=remove_top, n_bottom=remove_bot)
 print(model.bc.features.num_features)
 # %%
 model.bc.train_soldiers(data, fast=False)
 # %%
 model.w = np.zeros(model.bc.features.num_features)
-#%%
-result = model.perceptron(data, epochs=NUM_EPOCHS,validation=test)
-result['epochs'] += 10 * len(results_all)
+# %%
+result = model.perceptron(data, epochs=20, validation=test)
 results_all.append(result)
-#%%
+# %%
 df_results = pd.DataFrame(pd.concat(results_all))
 df_results.to_csv(f'{RESULTS_PATH}\\re_{time.strftime("%d_%b_%y_%S_%M_%H")}.csv')
 # model.w.tofile(f'{WEIGHTS_PATH}\\w_{time.strftime("%d_%b_%y_%S_%M_%H")}.h5')
